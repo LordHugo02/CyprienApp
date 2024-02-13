9c803b4503d8cc86b95acb50519e5eea5d38cd52
Creanics <csiaud83@gmail.com>
2024-02-13 08:51:03 +0100

    add id to CustomTableLine
    get id from "line" & pass it to handleModify to get item

--- src/components/gestion/CustomTableLine.tsx
+++ src/components/gestion/CustomTableLine.tsx
@@ -1,30 +1,40 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { EditorContext } from '../../contexts';
 
-interface ILine {
-  id: number,
+
+interface ILineContent {
   slug: string,
   content: string
+}
+interface ILine {
+  id: number,
+  content: ILineContent[]
 }
 interface IBaseLine {
   line: object,
   headers: object
 }
+interface ILineRef {
+  id: number
+}
 
 const CustomTableLine = ({ line, headers }: IBaseLine) => {
-  const [actualLine, setLine] = useState<ILine[]>([]);
+  const [actualLine, setLine] = useState<ILine>();
   const { toggleEditor } = useContext(EditorContext);
   
   const handleLineChanges = () => {
-    const tempTab: ILine[] = [];
+    const tempLine = line as ILineRef
+    const tempTab: ILine = {
+      id: tempLine.id,
+      content: []
+    };
+    
     const refTab = Object.keys(headers);
     const baseLinekeys = Object.keys(line);
     const baseLinevalues = Object.values(line);
     refTab.forEach(ref => {
       const iref = baseLinekeys.indexOf(ref);
-      const iid = baseLinekeys.indexOf('id ');
-      const temp: ILine = {
-        id: baseLinevalues[iid],
+      const temp: ILineContent = {
         slug: ref,
         content: baseLinevalues[iref],
       };
@@ -37,11 +47,11 @@ const CustomTableLine = ({ line, headers }: IBaseLine) => {
       } else if (temp.slug === 'rate') {
         temp.content = (parseInt(temp.content, 10) / 100) + ' %';
       }
-      tempTab.push(temp);
+      tempTab.content.push(temp);
     });
     setLine(tempTab);
   }
-  const handleModify = (ratio: number) => {
+  const handleModify = (id: number) => {
     toggleEditor(1)
   }
 
@@ -55,24 +65,28 @@ const CustomTableLine = ({ line, headers }: IBaseLine) => {
   const editLinkClass = `absolute text-blue bottom-2 left-2 flex gap-2 opacity-0 group-hover:opacity-100 transition-all`;
   const trClass = `bg-slate-100 even:bg-slate-200 relative cursor-default productTable group relative`;
   return (
-    <tr className={trClass}>
-      {Object.values(actualLine)
+    <>
+    {
+      actualLine && 
+      <tr className={trClass}>
+      {Object.values(actualLine.content)
         .map(
           (col) => {
-            console.log(col)
             return (
               <>
                 <td className='p-2 min-w-28 max-w-xl w-max pb-8'>{col.content}</td>
-                <div className={editLinkClass}>
-                  <div className='cursor-pointer' onClick={() => handleModify(1)}>Modifier</div>|
-                  <div className='cursor-pointer text-red-500'>Supprimer</div>
-                </div>
               </>
             );
           },
         )
       }
+      <p className={editLinkClass}>
+        <span className='cursor-pointer' onClick={() => handleModify(actualLine.id)}>Modifier</span>|
+        <span className='cursor-pointer text-red-500'>Supprimer</span>
+      </p>
     </tr>
+    }
+    </>
   );
 };
 
