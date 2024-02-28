8d22c3eb5de9b5408f3db74351d9f3e6c8c14dca
Creanics <csiaud83@gmail.com>
2024-02-28 08:42:19 +0100

    fix import file name

--- src/components/gestion/CustomTable.tsx
+++ src/components/gestion/CustomTable.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CustomTableLine from './CustomTableLine';
-import { IStorageProps } from '../../interfaces/Istorage';
+import { IStorageProps } from '../../interfaces/IStorage';
 
 const CustomTable = ({ lines, headers }: IStorageProps) => {
   const [sortRatio, setRatio] = useState(1);
